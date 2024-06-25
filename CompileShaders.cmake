b086bfe484e5f3fb586af6670d88ec3cc6d1a815
alpertunga-bile <kingdom19online@hotmail.com>
2024-06-25 10:12:19 +0300

    binary flag is added

--- CompileShaders.cmake
+++ CompileShaders.cmake
@@ -1,3 +1,5 @@
+include(Utilities.cmake)
+
 find_program(GLSL_VALIDATOR glslangValidator HINTS /usr/bin /usr/local/bin $ENV{VULKAN_SDK}/Bin/ $ENV{VULKAN_SDK}/Bin32/)
 find_program(SPIRV_OPT spirv-opt HINTS /usr/bin /usr/local/bin $ENV{VULKAN_SDK}/Bin/ $ENV{VULKAN_SDK}/Bin32/)
 
@@ -11,7 +13,6 @@ function(compile_glsl_shaders PROJECT_TARGET TARGET_ENV TARGET_ENV_OPT SHADER_DI
             "${SHADER_DIR}/*.task"
             "${SHADER_DIR}/*.tesc"
             "${SHADER_DIR}/*.tese"
-            "${SHADER_DIR}/*.glsl"
             "${SHADER_DIR}/*.geom"
             "${SHADER_DIR}/*.rgen"
             "${SHADER_DIR}/*.rint"
@@ -20,6 +21,14 @@ function(compile_glsl_shaders PROJECT_TARGET TARGET_ENV TARGET_ENV_OPT SHADER_DI
             "${SHADER_DIR}/*.rmiss"
             "${SHADER_DIR}/*.rcall"
         )
+
+        set(IS_VULKAN_ENV string_starts_with(${TARGET_ENV} vulkan))
+
+        if(IS_VULKAN_ENV EQUAL true)
+            set(GEN_BIN_FLAG -V)
+        else()
+            set(GEN_BIN_FLAG -G)
+        endif()
 
         foreach(GLSL_FILE ${GLSL_FILES})
             get_filename_component(GLSL_FILENAME ${GLSL_FILE} NAME_WE)
@@ -31,12 +40,12 @@ function(compile_glsl_shaders PROJECT_TARGET TARGET_ENV TARGET_ENV_OPT SHADER_DI
 
             add_custom_target(
                 ${GLSL_FILENAME}_SPIRV_TARGET
-                COMMAND ${GLSL_VALIDATOR} -V ${GLSL_FILE} -o ${SPIRV_FILE} -e main -t --enhanced-msgs --target-env ${TARGET_ENV} -I${SHADER_DIR} --spirv-val
+                COMMAND ${GLSL_VALIDATOR} ${GEN_BIN_FLAG} ${GLSL_FILE} -o ${SPIRV_FILE} -e main -t --enhanced-msgs --target-env ${TARGET_ENV} -I${SHADER_DIR} --spirv-val
             )
 
             add_custom_target(
                 ${GLSL_FILENAME}_SPIRV_C_TARGET
-                COMMAND ${GLSL_VALIDATOR} -V ${GLSL_FILE} -o ${SPIRV_C_HEADER_FILE} -e main -t --enhanced-msgs --target-env ${TARGET_ENV} -I${SHADER_DIR} --vn ${SPIRV_C_NAME} --spirv-val
+                COMMAND ${GLSL_VALIDATOR} ${GEN_BIN_FLAG} ${GLSL_FILE} -o ${SPIRV_C_HEADER_FILE} -e main -t --enhanced-msgs --target-env ${TARGET_ENV} -I${SHADER_DIR} --vn ${SPIRV_C_NAME} --spirv-val
             )
 
             add_dependencies(${PROJECT_TARGET} ${GLSL_FILENAME}_SPIRV_TARGET ${GLSL_FILENAME}_SPIRV_C_TARGET)
