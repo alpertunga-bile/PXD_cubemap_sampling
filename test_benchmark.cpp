151ea0221255ef04a73720f1c14b46309894a320
alpertunga-bile <kingdom19online@hotmail.com>
2024-06-25 11:27:35 +0300

    temp array is filled before tests

--- test_benchmark.cpp
+++ test_benchmark.cpp
@@ -1,34 +1,32 @@
 #include "test_funcs.hpp"
 
-#include <random>
-
 #include "benchmark.h"
 
-std::random_device dev;
-std::mt19937_64 random_engine(dev());
-std::uniform_real_distribution<> generator(-1000000.f, 1000000.f);
+glm::vec3 temp_vecs[N];
+const int result = pxd::fill_temp_array(temp_vecs);
 
 static void BM_ReelFunc(benchmark::State &state) {
+  size_t index = 0;
 
   for (auto _ : state) {
+    pxd::reel_func(temp_vecs[index]);
+
     state.PauseTiming();
-    glm::vec3 temp_vec =
-        glm::vec3(generator(random_engine), generator(random_engine),
-                  generator(random_engine));
+    ++index;
     state.ResumeTiming();
-    pxd::reel_func(temp_vec);
   }
 }
 BENCHMARK(BM_ReelFunc)->Name("Reel Cubemap Sampling")->Iterations(N);
 
 static void BM_RefactoredFunc(benchmark::State &state) {
+  size_t index = 0;
+
   for (auto _ : state) {
+    pxd::refactored_func(temp_vecs[index]);
+
     state.PauseTiming();
-    glm::vec3 temp_vec =
-        glm::vec3(generator(random_engine), generator(random_engine),
-                  generator(random_engine));
+    ++index;
     state.ResumeTiming();
-    pxd::refactored_func(temp_vec);
   }
 }
 BENCHMARK(BM_RefactoredFunc)
