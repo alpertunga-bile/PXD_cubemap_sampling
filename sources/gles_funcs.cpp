4f3c7719b710fa678737ae47830989e03bf93e00
alpertunga-bile <kingdom19online@hotmail.com>
2024-06-25 16:07:26 +0300

    shader creation is added

--- sources/gles_funcs.cpp
+++ sources/gles_funcs.cpp
@@ -1,22 +1,89 @@
 #include "gles_funcs.hpp"
 
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+
 #ifndef GLAD_GLES2_IMPLEMENTATION
 #define GLAD_GLES2_IMPLEMENTATION
 #endif
 
 #include "gles2.h"
 
-#include "temp_vert_spv.h"
+#include "core.h"
 
-#include <string>
+namespace pxd {
+GLuint get_shader_program() {
+  fmt::println("Vertex Shader");
+  GLuint vertex_shader =
+      get_compiled_shader("shaders/temp.vert", GL_VERTEX_SHADER);
+  fmt::println("Fragment Shader");
+  GLuint frag_shader =
+      get_compiled_shader("shaders/temp.frag", GL_FRAGMENT_SHADER);
 
-GLuint pxd::get_shader_program() {
-  GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  GLuint program = glCreateProgram();
 
-  glShaderBinary(1, &vertex_shader, GL_UNSIGNED_INT, temp_vert_spv,
-                 sizeof(temp_vert_spv) / sizeof(uint32_t));
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, frag_shader);
 
-  GLuint program = 0;
+  glLinkProgram(program);
+
+  glDetachShader(program, vertex_shader);
+  glDetachShader(program, frag_shader);
+
+  glDeleteShader(vertex_shader);
+  glDeleteShader(frag_shader);
 
   return program;
 }
+
+GLuint get_compiled_shader(const char *path, GLenum shader_type) {
+  if (std::filesystem::exists(path) == false) {
+    fmt::println("{} is not exists", path);
+    return 0;
+  }
+
+  std::ifstream reader;
+  std::string shader_source;
+  reader.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+  try {
+    reader.open(path);
+
+    std::stringstream stream;
+    stream << reader.rdbuf();
+    shader_source = stream.str();
+    reader.close();
+  } catch (std::ifstream::failure e) {
+    if (reader.is_open()) {
+      reader.close();
+    }
+  }
+
+  GLuint shader = glCreateShader(shader_type);
+
+  const GLchar *code_arr[] = {shader_source.c_str()};
+
+  glShaderSource(shader, 1, code_arr, NULL);
+
+  glCompileShader(shader);
+
+  check_shader(shader);
+
+  return shader;
+}
+
+void check_shader(GLuint shader) {
+  int success;
+  char info_log[512];
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+  if (success) {
+    return;
+  }
+
+  glGetShaderInfoLog(shader, 512, NULL, info_log);
+  fmt::println("::Shader Error:: {}", info_log);
+}
+
+} // namespace pxd
--- sources/gles_funcs.hpp
+++ sources/gles_funcs.hpp
@@ -4,4 +4,8 @@
 
 namespace pxd {
 GLuint get_shader_program();
-}
+
+GLuint get_compiled_shader(const char *path, GLenum shader_type);
+
+void check_shader(GLuint shader);
+} // namespace pxd
